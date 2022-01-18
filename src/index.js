25e51f3133c33b389258ee32f1ab026360c28c16
SunniDaze7 <pnykii98@gmail.com>
2022-01-18 17:52:56 -0600

    add specific movement functionality to pacman

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const layout = [
 1,1,1,1,0,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,0,1,1,1,1,
 1,1,1,1,0,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,0,1,1,1,1,
 1,1,1,1,0,1,1,4,4,4,4,4,4,4,4,4,4,4,4,4,4,1,1,0,1,1,1,1,
-1,1,1,1,0,1,1,4,1,1,1,1,1,1,4,1,1,1,1,1,4,1,1,0,1,1,1,1,
+1,1,1,1,0,1,1,4,1,1,1,1,1,1,2,1,1,1,1,1,4,1,1,0,1,1,1,1,
 1,1,1,1,0,1,1,4,1,1,1,2,2,2,2,2,2,2,1,1,4,1,1,0,1,1,1,1,
 4,4,4,4,4,4,4,4,1,1,1,2,2,2,2,2,2,2,1,1,4,4,4,4,4,4,4,4,
 1,1,1,1,0,1,1,4,1,1,1,2,2,2,2,2,2,2,1,1,4,1,1,0,1,1,1,1,
@@ -58,6 +58,8 @@ function createBoard() {
             squares[i].classList.add('wall')
         } else if(layout[i] === 3) {
             squares[i].classList.add('power-pellet')
+        }else if(layout[i] === 2) {
+            squares[i].classList.add('ghost-lair')
         }
     } 
 }
@@ -75,18 +77,29 @@ function movePacman(e){
 
     switch(e.keyCode){
         case 37:
-            if(pacmanCurrentIndex % width !== 0) pacmanCurrentIndex -=1
+            if(pacmanCurrentIndex % width !== 0 && !squares[pacmanCurrentIndex -1].classList.contains("wall") && !squares[pacmanCurrentIndex -1].classList.contains("ghost-lair")) 
+            pacmanCurrentIndex -=1
             break
         case 38:
-            if(pacmanCurrentIndex % width !== 0) pacmanCurrentIndex -=width
+            if(pacmanCurrentIndex - width >= 0 && !squares[pacmanCurrentIndex -width].classList.contains("wall") && !squares[pacmanCurrentIndex -width].classList.contains("ghost-lair")) 
+            pacmanCurrentIndex -=width
             break
         case 39:
-            if(pacmanCurrentIndex % width !== 0) pacmanCurrentIndex +=1
+            if(pacmanCurrentIndex % width < width -1 && !squares[pacmanCurrentIndex +1].classList.contains("wall") && !squares[pacmanCurrentIndex +1].classList.contains("ghost-lair")) 
+            pacmanCurrentIndex +=1
             break
         case 40:
-            if(pacmanCurrentIndex % width !== 0) pacmanCurrentIndex +=width
+            if(pacmanCurrentIndex + width < width *  width && !squares[pacmanCurrentIndex + width].classList.contains("wall") && !squares[pacmanCurrentIndex + width].classList.contains("ghost-lair")) 
+            pacmanCurrentIndex +=width
             break
     }
 
-    squares[pacmanCurrentIndex].classList.add()
+    squares[pacmanCurrentIndex].classList.add('pac-man')
+
+    //pacDotEaten()
+    //powerPelletEaten()
+    //checkforGameOver()    
+    //checkForWin()
+
 }
+document.addEventListener("keyup", movePacman)
